4a96eb72dcd7bdecf9d668450328add84b834a1e
jesse espinola <jespinola777>
2023-03-31 16:27:21 -0600

    commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-let sNum = Math.floor(Math.random() * 10) + 1;
 let userName = prompt("Please enter your Name.")
+let playAgain = true
+while(playAgain){
+let sNum = Math.floor(Math.random() * 10) + 1;
 let input = prompt(`${userName}, guess a number between 1 and 10`)
 let numOfGuess = [];
 if(!isNaN(input)){
@@ -10,10 +12,10 @@ let counter = 1;
 
 while(number !== sNum){
 if (Number.isNaN(number)){
-        alert("Give a valid number please");
-        number = Number(prompt("Guess again"));
-        numOfGuess.push(number);
-        counter++;
+    alert("Give a valid number please");
+    number = Number(prompt("Guess again"));
+    numOfGuess.push(number);
+    counter++;
     }
 if (number < sNum){
     number = Number(prompt('Guess Higher'));
@@ -29,4 +31,14 @@ if (number < sNum){
 if (number === sNum){
     alert(`Congrats ${userName}, you are Correct! You got it in ${counter} guess, your previous guess were ${numOfGuess}!`)
 }
+playAgain = prompt('Do you want to play again? If so, type \'yes\'.')
+if(playAgain == 'yes'){
+    playAgain = true
+}else{
+    alert('Thanks for playing!')
+    playAgain = false
+}
+}
+
+
  
