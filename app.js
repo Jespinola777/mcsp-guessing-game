83f1bcea72daff464d4ad0256405f4198174dec0
jesse espinola <jespinola777>
2023-04-04 11:47:50 -0600

    final product

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-let userName = prompt("Please enter your Name.")
-let playAgain = true
-while(playAgain){
+let userName = prompt("Please enter your Name.");
+let playAgain = true;
+let history = {};
+if(userName == null){
+    alert('Bye, thanks for not playing')
+    playAgain = false
+}
+
+while(playAgain === true){ //play again loop
 let sNum = Math.floor(Math.random() * 10) + 1;
-let input = prompt(`${userName}, guess a number between 1 and 10`)
+let input = prompt(`${userName}, guess a number between 1 and 10`);
+if(input == null){
+    alert('Bye, thanks for not playing')
+    break;
+}
 let numOfGuess = [];
 if(!isNaN(input)){
 numOfGuess.push(input);
@@ -11,6 +21,10 @@ let number = Number(input);
 let counter = 1;
 
 while(number !== sNum){
+    if(number == null || number === 0){
+        alert('Bye, thanks for not playing');
+        break;
+    }
 if (Number.isNaN(number)){
     alert("Give a valid number please");
     number = Number(prompt("Guess again"));
@@ -25,14 +39,39 @@ if (number < sNum){
     number = Number(prompt('Guess Lower'));
     numOfGuess.push(number);
     counter++;
+    }
 }
+if(number == null || number === 0){
+    break;
+}
+//feature 7
+if (history[userName] !== undefined) {
+    let numGuesses = numOfGuess.length; 
+    let previousGuesses = history[userName].length;
+        if (number === sNum && previousGuesses > numGuesses){
+        let scoreBeat = previousGuesses - numGuesses;
+    alert(`That's Correct ${userName}, you beat your previous score by ${scoreBeat}. `);
+    }
+    else if (number === sNum && numGuesses < previousGuesses){
+        let scoreBeat = numGuesses - previousGuesses;
+        alert(`That's Correct ${userName}, you got ${scoreBeat} better last time.`);
+    } else { 
+        alert(`That's Correct ${userName}, you got the same score!`);
+}
+} else {
+alert(`Congrats ${userName}, you are Correct! You got it in ${counter} guess, your previous guesses were ${numOfGuess}!`);
 }
 
-if (number === sNum){
-    alert(`Congrats ${userName}, you are Correct! You got it in ${counter} guess, your previous guess were ${numOfGuess}!`)
+if (number == null) {
+    break;
 }
-playAgain = prompt('Do you want to play again? If so, type \'yes\'.')
-if(playAgain == 'yes'){
+
+history[userName];
+history[userName] = numOfGuess;
+
+
+playAgain = prompt('Do you want to play again?')
+if(playAgain.toLowerCase() == 'yes'){
     playAgain = true
 }else{
     alert('Thanks for playing!')
